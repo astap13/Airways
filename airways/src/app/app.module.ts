7b73c55d10e7624f579710e97b44fd8569195908
Kiryl Astapenka <81929875+astap13@users.noreply.github.com>
2023-06-05 18:06:50 +0200

    feat: update version

--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -43,13 +43,12 @@ import { CoreModule } from './core/core.module';
     FormsModule,
     ReactiveFormsModule,
     StoreModule.forRoot({}),
-    // Instrumentation must be imported after importing StoreModule (config is optional)
     StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
     }),
   ],
 })
