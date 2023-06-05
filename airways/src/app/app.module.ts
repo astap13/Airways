2e51311faa7b1c6631d7e1f3fab5eebb1a6eb0c0
Kiryl Astapenka <81929875+astap13@users.noreply.github.com>
2023-06-05 17:30:04 +0200

    feat: add ngrx

--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgFor } from '@angular/common';
 /* eslint-disable import/no-extraneous-dependencies */
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +10,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbDropdownModule, NgbModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,6 +42,15 @@ import { CoreModule } from './core/core.module';
     NgbTypeaheadModule,
     FormsModule,
     ReactiveFormsModule,
+    StoreModule.forRoot({}),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
   ],
 })
 export class AppModule {}
