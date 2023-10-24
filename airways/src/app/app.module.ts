e4327eca86ec4a30830e158224aab7f8adddf9c4
Kiryl Astapenka <astapenko.1304@gmail.com>
2023-10-24 05:28:43 +0200

    feat: add firebase for autentification

--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgFor } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 /* eslint-disable import/no-extraneous-dependencies */
 import { isDevMode, NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,14 +24,28 @@ import {
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './auth/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { VerifyEmailComponent } from './auth/verify-email/verify-email.component';
 import { BookingModule } from './booking/booking.module';
 import { CoreComponent } from './core/core.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, CoreComponent],
+  declarations: [
+    AppComponent,
+    CoreComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   imports: [
@@ -50,6 +69,11 @@ import { CoreModule } from './core/core.module';
     ReactiveFormsModule,
     NgbDatepickerModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
