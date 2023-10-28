455e68af2e67f7cbd54d762250625a0b16731ed2
Kiryl Astapenka <astapenko.1304@gmail.com>
2023-10-28 06:03:29 +0200

    feat:add pakets

--- airways/src/environments/environment.ts
+++ airways/src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
+    projectId: 'airways-a8ed2',
+    appId: '1:527287112935:web:9d5fd93650e425ab967e42',
+    storageBucket: 'airways-a8ed2.appspot.com',
     apiKey: 'AIzaSyDxmqBpTzoXRSJ8dsgR3viR4cRt2js2Lew',
     authDomain: 'airways-a8ed2.firebaseapp.com',
-    projectId: 'airways-a8ed2',
-    storageBucket: 'airways-a8ed2.appspot.com',
     messagingSenderId: '527287112935',
-    appId: '1:527287112935:web:9d5fd93650e425ab967e42',
+    measurementId: 'G-PSP4L9P0MF',
   },
 };
